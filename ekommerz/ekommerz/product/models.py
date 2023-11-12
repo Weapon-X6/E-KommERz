826a8f0150b2e4956777d501d10ad11f86c11990
waffe_x20 <argueta_eduardo@yahoo.de>
2023-11-11 22:57:56 -0600

    Add custom manager for product

--- ekommerz/ekommerz/product/models.py
+++ ekommerz/ekommerz/product/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
 
+class ActiveManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
+
 class Category(MPTTModel):
     name = models.CharField(max_length=100, unique=True)
     parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
@@ -30,6 +35,9 @@ class Product(models.Model):
         "Category", on_delete=models.SET_NULL, null=True, blank=True
     )
     is_active = models.BooleanField(default=False)
+
+    isactive = ActiveManager()
+    objects = models.Manager()
 
     def __str__(self):
         return self.name
